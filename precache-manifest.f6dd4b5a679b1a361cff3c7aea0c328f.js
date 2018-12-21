0372aa912e2ef6871d5b91b22df6e61c9365cbcb
denish rana <denishrana09@gmail.com>
2018-12-21 15:12:56 +0530

    feat: build created

---
+++ precache-manifest.f6dd4b5a679b1a361cff3c7aea0c328f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "89d234d8d2c064f289191f1c3c521111",
+    "url": "./static/media/product2.89d234d8.jpeg"
+  },
+  {
+    "revision": "af509add6172dd8fc943",
+    "url": "./static/css/main.c3e485c2.chunk.css"
+  },
+  {
+    "revision": "5a5b4526a144d0e0bdc5",
+    "url": "./static/js/1.5a5b4526.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a26019dc4207960ecc86f8e1ca02d7ea",
+    "url": "./static/media/sofa1.a26019dc.jpg"
+  },
+  {
+    "revision": "65354306307548a4f17dab97c7efd1c4",
+    "url": "./static/media/sofa2.65354306.jpg"
+  },
+  {
+    "revision": "c5507e5c0c3993370b9802465a3ed784",
+    "url": "./static/media/sofa3.c5507e5c.jpg"
+  },
+  {
+    "revision": "fc92046553e4687202013a634b052562",
+    "url": "./static/media/product10.fc920465.jpeg"
+  },
+  {
+    "revision": "92db1fbe9f891237c92845706dc3c986",
+    "url": "./static/media/product1.92db1fbe.jpeg"
+  },
+  {
+    "revision": "af509add6172dd8fc943",
+    "url": "./static/js/main.af509add.chunk.js"
+  },
+  {
+    "revision": "37a86f0fe3da85179efa3bedba7fd577",
+    "url": "./static/media/product3.37a86f0f.jpeg"
+  },
+  {
+    "revision": "6dfadda99eaed85b888a8ef06e8cc396",
+    "url": "./static/media/product4.6dfadda9.jpeg"
+  },
+  {
+    "revision": "f911c401036517a031b769f4b824074c",
+    "url": "./static/media/product5.f911c401.jpeg"
+  },
+  {
+    "revision": "a8b7d7830d9b4d3d6a82aa1bf6daab6e",
+    "url": "./static/media/product6.a8b7d783.jpeg"
+  },
+  {
+    "revision": "6d1793e100cfc415887ffd33331dac5a",
+    "url": "./static/media/product7.6d1793e1.jpeg"
+  },
+  {
+    "revision": "d96aa40901bc668e8e2662da3d098b74",
+    "url": "./static/media/product8.d96aa409.jpeg"
+  },
+  {
+    "revision": "c606534591759bcc2177367251e2ca90",
+    "url": "./static/media/product9.c6065345.jpeg"
+  },
+  {
+    "revision": "fbb0b9366296fb77b169761e7a7db053",
+    "url": "./index.html"
+  }
+];
